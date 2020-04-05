c7c9c18e3ed33ac34d6341757dfdf7c1797dcdfe
Nair <37509104+hellonair@users.noreply.github.com>
2020-04-05 12:50:34 +0530

    Bug fix with resolv.conf

--- ipamsrv/root/gitsync.sh
+++ ipamsrv/root/gitsync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+cd /
 curl https://raw.githubusercontent.com/hellonair/dot-ok-home-labs/master/ipamsrv/etc/bind/named.conf.local -o /etc/bind/named.conf.local
 curl https://raw.githubusercontent.com/hellonair/dot-ok-home-labs/master/ipamsrv/etc/bind/named.conf.options -o /etc/bind/named.conf.options
 curl https://raw.githubusercontent.com/hellonair/dot-ok-home-labs/master/ipamsrv/etc/bind/zones/db.168.192 -o /etc/bind/zones/db.168.192
 curl https://raw.githubusercontent.com/hellonair/dot-ok-home-labs/master/ipamsrv/etc/bind/zones/db.ok.com -o /etc/bind/zones/db.ok.com
+cd /
+rm /etc/resolv.conf
+# Bug fix with NXDOMAIN error
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.confresolv.conf /etc/resolv.conf
 service bind9 restart
