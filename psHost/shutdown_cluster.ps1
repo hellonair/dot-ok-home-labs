3ec2f1c15af871235b3c5935341d490083403bdf
Nair <hellonair@gmail.com>
2020-04-26 15:53:24 +0530

    Cluster creation and shutdown
    Create v7compute cluster
    Shutdown all hosts in cluster

---
+++ psHost/shutdown_cluster.ps1
@@ -0,0 +1,38 @@
+﻿<#
+
+.SYNOPSIS
+This is a powershell script to shutdown ESxi clusters for OK labs.
+
+.DESCRIPTION
+Shutdown all ESXi hosts in the cluster
+
+
+.EXAMPLE
+./shutdown_cluster.ps1 -clusterName <cluster Name>
+
+.NOTES
+Developed for hellonair's ok labs server builds and can be used with pre-conditions which are not listed here
+
+.AUTHOR
+Hari Nair aka hellonair
+#>
+
+param(
+ 
+     [Parameter()]
+     [string]$clusterName
+ 
+ )
+
+# Infrastructure
+$vcsa = "dc1vcsa70.ok"
+$dc = "DC1"
+# Credentials
+$vcAdmin = "Administrator@vsphere.local"
+$esxiAdmin = "root"
+$passWord = "Passw0rd!"
+# Process
+Connect-VIServer -server $vcsa -User $vcAdmin -Password $passWord
+Get-Cluster $clusterName |Get-VMHost
+Get-Cluster $clusterName |Get-vmhost | Set-VMHost -State Maintenance –VsanDataMigrationMode NoDataMigration
+Get-Cluster $clusterName |Get-VMHost | Stop-VMHost -Confirm:$false -Force
